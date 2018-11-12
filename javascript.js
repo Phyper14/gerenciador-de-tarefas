eac1beb90d667ee6a845084962e07ca97ce049fa
devjsilva <joao.silva1611@etec.sp.gov.br>
2018-11-12 20:25:51 -0200

    Adicionamos as cores

--- javascript.js
+++ javascript.js
@@ -1,3 +1,11 @@
-var botao = document.querySelector("#botao");
-var postit = document.getElementById('tarefa').value;
-console.log(postit);
+var btnSalvar = document.querySelector("#btnSalvar");
+btnSalvar.addEventListener("click", function(event){
+	event.preventDefault();
+	var corpostit = document.getElementById("cor");
+	var tarefa = document.getElementById("tarefa").value;
+	var postit = document.createElement("p");
+	postit.appendChild(tarefa);
+	body.appendChild(postit);
+
+	console.log(postit);
+});
