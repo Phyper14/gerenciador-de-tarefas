405516d36525f0b4ba9553a2129ed258ea8095b2
devjsilva <joao.silva1611@etec.sp.gov.br>
2018-11-23 22:08:05 -0200

    Definido tamanho das notas, adicionado cores às notas, definido enclinação e definido espaçamento entre as notas

--- javascript.js
+++ javascript.js
@@ -1,16 +1,18 @@
 var btnSalvar = document.querySelector("#btnSalvar");
 btnSalvar.addEventListener("click", function(event){
 	event.preventDefault();
-	var corpostit = document.getElementById("cor");
+	var corpostit = document.getElementById("cor").value;
 	var tarefa = document.getElementById("tarefa").value;
-	var corpo = document.querySelector("body");
+	var corpo = document.getElementById("lista_tarefas");
 	var postit = document.createElement("p");
+	postit.style.backgroundColor = corpostit;
+	console.log(corpostit);
 	postit.textContent = tarefa
+	postit.classList.add("caixa");
 	corpo.appendChild(postit);
 });
 function cortarefas(){
-	var classe = document.getElementByClass("cor").className;
-	console.log(classe);
+	
 	/*if (class == rosa){
 		console.log("Rosa");
 	}*/
