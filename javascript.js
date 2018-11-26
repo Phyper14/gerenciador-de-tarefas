0987ad6b2d3d169bce81beaad347863dc20550cf
devjsilva <joao.silva1611@etec.sp.gov.br>
2018-11-26 21:24:06 -0200

    Adicionamos botões de excluir e editar

--- javascript.js
+++ javascript.js
@@ -1,22 +1,36 @@
 var btnSalvar = document.querySelector("#btnSalvar");
 btnSalvar.addEventListener("click", function(event){
 	event.preventDefault();
+	var nota = document.createElement("div");
+	var btnApagar = criarbtn("cor-apagar", "btn-apagar");
+	var btnEditar = criarbtn("cor-editar","btn-editar");
 	var corpostit = document.getElementById("cor").value;
 	var tarefa = document.getElementById("tarefa").value;
 	var corpo = document.getElementById("lista_tarefas");
 	var postit = document.createElement("p");
 	var fonte = corpostit.replace("#", '');
-	var r = parseInt
-	postit.style.backgroundColor = corpostit;
+	var r = parseInt(fonte.substring(0,2), 16);
+	var g = parseInt(fonte.substring(2,4), 16);
+	var b = parseInt(fonte.substring(4,6), 16);
+	console.log(r, g, b)
+	nota.style.backgroundColor = corpostit;
 	console.log(corpostit);
 	postit.textContent = tarefa
-	postit.classList.add("caixa");
-	corpo.appendChild(postit);
+	nota.classList.add("caixa");
+	nota.classList.add("notas");
+	postit.appendChild(btnApagar);
+	postit.appendChild(btnEditar);
+	nota.appendChild(postit);
+	corpo.appendChild(nota);
 });
-function cortarefas(){
+function criarbtn(cor, icone){
+	var i = document.createElement("i");
+	var btn = document.createElement("button");
+	btn.appendChild(i);
+	btn.classList.add(cor);
+	i.classList.add(icone);
+	return btn;
+}
+/**function cortarefas(){
 	
-	/*if (class == rosa){
-		console.log("Rosa");
-	}*/
-};
-
+};*/
